9526aa5ce3821de0b9715a1bfab0405ba25326d9
olsdavis <olsdavis@gmail.com>
2021-10-15 08:33:42 +0100

    fix configs

--- website/config.py
+++ website/config.py
@@ -6,26 +6,21 @@ class Config:
     """
     Represents different configurations of the app.
     """
-    def __init__(self, debug, testing, sql_alchemy_track_modifications, sql_alchemy_database_uri = 'sqlite:///test.db'):
-        """
-        :param debug: Debug mode (boolean)
-        :param testing: Testing mode (boolean)
-        :param sql_alchemy_track_modifications: Enables SQLAlchemy to track modifications on the database (boolean)
-        :param sql_alchemy_database_uri: The URI towards the database (string)
-        """
-        assert type(debug) == bool
-        assert type(testing) == bool
-        assert type(sql_alchemy_track_modifications) == bool
-        assert type(sql_alchemy_database_uri) == str
+    DEBUG = True
+    TESTING = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
 
-        self.debug = debug
-        self.testing = testing
-        self.sql_alchemy_database_uri = sql_alchemy_database_uri
-        self.sql_alchemy_track_modifications = sql_alchemy_track_modifications
+class TestConfig(Config):
+    Testing = True
+
+class ProductionConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 config_by_name = {
-    'development': Config(debug = True, testing = False, sql_alchemy_track_modifications = True),
-    'testing': Config(debug = True, testing = True, sql_alchemy_track_modifications = True),
-    'production': Config(debug = False, testing = False, sql_alchemy_track_modifications = False),
-    'default': Config(debug = True, testing = False, sql_alchemy_track_modifications = True)
+    'development': Config,
+    'testing': TestConfig,
+    'production': ProductionConfig,
+    'default': Config
 }
