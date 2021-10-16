16e46884508c5aa4dbed5229730785b7692090db
boanzhu <bz2818@ic.ac.uk>
2021-10-16 18:55:55 +0100

    create branch and import bootstrap-vue

--- templates/src/components/Home.vue
+++ templates/src/components/Home.vue
@@ -1,32 +1,51 @@
 <template>
-  <div class="container">
-    <button type="button" class="btn btn-primary">{{ msg }}</button>
-  </div>
+  <b-container fluid>
+    <div class="container">
+      <button type="button" class="btn btn-primary">Import text</button>
+      <button type="button" class="btn btn-primary">Upload image</button>
+
+      <b-row>
+        <b-col sm="1">
+          <button type="button" class="btn btn-primary">Select words</button>
+        </b-col>
+        <b-col sm="5">
+          <b-form-textarea
+            id="textarea-auto-height"
+            placeholder="Auto height textarea"
+            rows="3"
+            max-rows="8"
+          ></b-form-textarea>
+        </b-col>
+      </b-row>
+      
+    </div>
+  </b-container>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 
 export default {
-  name: 'Home',
+  name: "Home",
   data() {
     return {
-      msg: '',
-    }
+      msg: "",
+      text: "",
+    };
   },
   methods: {
-    getMessage() {
-      axios.get(this.$hostname)
-        .then((res) => {
-          this.msg = res.data
-        })
-        .catch((error) => {
-          console.error(error) // TODO: handle error correctly
-        })
-    },
+    // getMessage() {
+    //   axios.get(this.$hostname)
+    //     .then((res) => {
+    //       this.msg = res.data
+    //     })
+    //     .catch((error) => {
+    //       console.error(error) // TODO: handle error correctly
+    //     })
+    // },
   },
   created() {
-    this.getMessage()
+    // this.getMessage()
   },
-}
+};
 </script>
