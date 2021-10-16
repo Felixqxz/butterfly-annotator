d44b12532c0a8d769859ac34519f37a551e9c48d
Xingzhi Qian <xq219@ic.ac.uk>
2021-10-16 19:17:07 +0100

    update login pages

--- templates/src/components/Login.vue
+++ templates/src/components/Login.vue
@@ -1,29 +1,59 @@
 <template>
-  <div>
-    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-      <b-form-group id="input-group-1" label="Username:" label-for="input-1">
-        <b-form-input
-          id="input-1"
-          v-model="form.username"
-          required
-          placeholder="Enter username"
-        ></b-form-input>
-      </b-form-group>
+  <div class="login_table">
+    <b-card bg-variant="light">
+      <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+        <b-form-group
+          label-cols-lg="3"
+          label="Please Sign In"
+          label-align-lg="center"
+          label-size="lg"
+          label-class="font-weight-bold pt-0"
+          class="mb-0"
+        >
+          <br />
+          <b-form-group
+            label-cols-sm="3"
+            label-align-sm="center"
+            id="input-group-1"
+            label="Username:"
+            label-for="input-1"
+          >
+            <b-form-input
+              id="input-1"
+              v-model="form.username"
+              required
+              placeholder="Enter username"
+            ></b-form-input>
+          </b-form-group>
 
-      <b-form-group id="input-group-2" label="Password:" label-for="input-2">
-        <b-form-input
-          id="input-2"
-          v-model="form.password"
-          required
-          placeholder="Enter password"
-        ></b-form-input>
-      </b-form-group>
+          <b-form-group
+            label-cols-sm="3"
+            label-align-sm="center"
+            id="input-group-2"
+            label="Password:"
+            label-for="input-2"
+          >
+            <b-form-input
+              id="input-2"
+              v-model="form.password"
+              type="password"
+              required
+              placeholder="Enter password"
+            ></b-form-input>
+          </b-form-group>
 
-      <b-button type="submit" variant="primary">Login</b-button>
-      <b-button type="reset" variant="danger">Reset</b-button>
-    </b-form>
-    <br>
-    <b-button variant="outline-primary" href="/register">Register if you don't have an account</b-button>
+          <b-button type="submit" variant="primary">Sign in</b-button
+          >&nbsp;&nbsp;
+          <b-button type="reset" variant="danger">Reset</b-button>
+        </b-form-group>
+      </b-form>
+
+      <br />
+
+      <b-button variant="outline-primary" href="/register"
+        >Need an account?</b-button
+      >
+    </b-card>
   </div>
 </template>
 
@@ -41,10 +71,11 @@ export default {
   methods: {
     onSubmit(evt) {
       evt.preventDefault();
-      console.log(this.form)
+      alert(JSON.stringify(this.form));
     },
     onReset(evt) {
       evt.preventDefault();
+      // Reset our form values
       this.form.username = "";
       this.form.password = "";
       this.show = false;
@@ -55,3 +86,10 @@ export default {
   },
 };
 </script>
+
+<style>
+.login_table {
+  margin: 50px 100px;
+  text-align: center;
+}
+</style>
--- templates/src/components/Register.vue
+++ templates/src/components/Register.vue
@@ -1,53 +1,89 @@
 <template>
-  <div>
+  <div class="login_table">
     <b-card bg-variant="light">
-        <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <b-form @submit="onSubmit" @reset="onReset" v-if="show">
         <b-form-group
-            label-cols-lg="3"
-            label="Please Register"
-            label-align-lg="center"
-            label-class="font-weight-bold pt-0"
-            class="mb-0"
+          label-cols-lg="3"
+          label="Please Register"
+          label-align-lg="center"
+          label-size="lg"
+          label-class="font-weight-bold pt-0"
+          class="mb-0"
         >
-        <br>
-            <b-form-group
-                label-cols-sm="3" label-align-sm="center" id="input-group-1" label="Username:" label-for="input-1">
-                <b-form-input
-                id="input-1"
-                v-model="form.username"
-                required
-                placeholder="Enter username"
-                ></b-form-input>
-            </b-form-group>
+          <br />
+          <b-form-group
+            label-cols-sm="3"
+            label-align-sm="center"
+            id="input-group-1"
+            label="Username:"
+            label-for="input-1"
+          >
+            <b-form-input
+              id="input-1"
+              v-model="form.username"
+              required
+              placeholder="Enter username"
+            ></b-form-input>
+          </b-form-group>
 
-            <b-form-group label-cols-sm="3" label-align-sm="center" id="input-group-2" label="Password:" label-for="input-2">
-                <b-form-input
-                id="input-2"
-                v-model="form.password"
-                required
-                placeholder="Enter password"
-                ></b-form-input>
-            </b-form-group>
+          <b-form-group
+            label-cols-sm="3"
+            label-align-sm="center"
+            id="input-group-2"
+            label="Password:"
+            label-for="input-2"
+          >
+            <b-form-input
+              id="input-2"
+              v-model="form.password"
+              type="password"
+              required
+              placeholder="Enter password"
+            ></b-form-input>
+          </b-form-group>
 
-            <b-form-group label-cols-sm="3" label-align-sm="center" id="input-group-3" label="Confirm password:" label-for="input-3">
-                <b-form-input
-                id="input-3"
-                v-model="form.confirmedPassword"
-                required
-                placeholder="Enter password again"
-                ></b-form-input>
-            </b-form-group>
+          <b-form-group
+            label-cols-sm="3"
+            label-align-sm="center"
+            id="input-group-3"
+            label="Confirm password:"
+            label-for="input-3"
+          >
+            <b-form-input
+              id="input-3"
+              v-model="form.confirmedPassword"
+              type="password"
+              required
+              placeholder="Enter password again"
+            ></b-form-input>
+          </b-form-group>
 
-            <b-button type="submit" variant="primary">Register</b-button>
-            <b-button type="reset" variant="danger">Reset</b-button>
+          <b-form-group
+            label-cols-sm="3"
+            label-align-sm="center"
+            id="input-group-4"
+            label="Email:"
+            label-for="input-4"
+          >
+            <b-form-input
+              id="input-4"
+              v-model="form.email"
+              type="email"
+              required
+              placeholder="Enter email"
+            ></b-form-input>
+          </b-form-group>
+
+          <b-button type="submit" variant="primary">Register</b-button>&nbsp;&nbsp;
+          <b-button type="reset" variant="danger">Reset</b-button>
         </b-form-group>
-            </b-form>
-       
+      </b-form>
 
-        <br>
-        
-        <b-button variant="outline-primary" href="/login">Login if you have an account</b-button>
-        
+      <br />
+
+      <b-button variant="outline-primary" href="/login"
+        >Already have an account?</b-button
+      >
     </b-card>
   </div>
 </template>
@@ -60,6 +96,7 @@ export default {
         username: "",
         password: "",
         confirmedPassword: "",
+        email: ""
       },
       show: true,
     };
@@ -75,6 +112,7 @@ export default {
       this.form.username = "";
       this.form.password = "";
       this.form.confirmedPassword = "";
+      this.form.email = "";
       // Trick to reset/clear native browser form validation state
       this.show = false;
       this.$nextTick(() => {
@@ -84,3 +122,10 @@ export default {
   },
 };
 </script>
+
+<style>
+.login_table {
+  margin: 50px 100px;
+  text-align: center;
+}
+</style>
