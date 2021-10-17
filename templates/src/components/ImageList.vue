8cc56b733627c05d7708288b46c81e8445827379
olsdavis <olsdavis@gmail.com>
2021-10-17 17:13:37 +0100

    responsive list of images of bank with animation

--- templates/src/components/ImageList.vue
+++ templates/src/components/ImageList.vue
@@ -1,14 +1,19 @@
 <template>
-  <div>
-    <div class="jumbotron">
-      <h1>Test!</h1>
-      <h2>This is bank {{ bankName }}</h2>
-    </div>
-    <div class="container container-fluid">
-      <figure class="figure col col-md-4 col-sm-6 col-xs-12 no-drag" v-for="image in images" v-bind:key="image.id">
-        <img :src=image.url class="figure-img img-fluid rounded" :alt=image.id />
-        <figcaption class="figure-caption text-center">{{ image.fullDescription }}</figcaption>
-      </figure>
+  <div class="container">
+    <h2 class="text-center">Bank: {{ bankName }}</h2>
+    <div class="row">
+      <div class="col-md-4 col-sm-6 col-xs-12" v-for="(image, index) in images" v-bind:key="image.id">
+        <div class="card no-drag image-to-annotate">
+          <div class="image-hover-container">
+            <img :src=image.url class="card-img-top image-hover"
+                 :style="'animation-delay:' + Math.floor(index / 3) * 100 + 'ms'"
+                 :alt=image.id />
+          </div>
+          <div class="card-body">
+            <span class="card-text">{{ image.fullDescription }}</span>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -27,18 +32,18 @@ export default {
   methods: {
     fetchImageList() {
       axios.get(this.$hostname + '/api/bank/' + this.$route.params.bankId)
-        .then(res => {
-          if (res.status !== 200) {
-            console.log('Could not load DB => ERROR, HTTP status=' + res.status) // TODO: handle correctly
-          } else {
-            let data = res.data
-            this.images = data.images
-            this.bankName = data.bankName
-          }
-        })
-        .catch(err => {
-          console.log(err) // TODO: handle errors properly
-        })
+          .then(res => {
+            if (res.status !== 200) {
+              console.log('Could not load DB => ERROR, HTTP status=' + res.status) // TODO: handle correctly
+            } else {
+              let data = res.data
+              this.images = data.images
+              this.bankName = data.bankName
+            }
+          })
+          .catch(err => {
+            console.log(err) // TODO: handle errors properly
+          })
     },
   },
   created() {
