d3807d43a86d9eeb0634e02269596a4be22d28c9
yc1319 <yc1319@imperial.ac.uk>
2021-10-17 10:41:55 +0100

    created simple footer component; footer now stays at the bottom of any page.

--- templates/src/components/Home.vue
+++ templates/src/components/Home.vue
@@ -1,16 +1,20 @@
 <template>
   <div>
     <Header></Header>
+    <div class="container"></div>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import Header from '../components/Header.vue'
+import Footer from '../components/Footer.vue'
 
 export default {
   name: 'Home',
   components: {
-    Header
+    Header,
+    Footer
   }
 }
 </script>
