f028686bc4a8f907ec3434929dc0a9fd8eb3412c
yc1319 <yc1319@imperial.ac.uk>
2021-10-17 09:40:05 +0100

    change navbar color to match login/register page

--- templates/src/components/Header.vue
+++ templates/src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <b-navbar toggleable="lg" type="dark" variant="dark">
+    <b-navbar toggleable="lg" type="dark" variant="info">
       <b-navbar-brand href="#">Butterfly annotator</b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
