021b4ed25c0ff0b4924662052200812907959a18
yc1319 <yc1319@imperial.ac.uk>
2021-10-17 09:47:57 +0100

    clean up Home.vue

--- templates/src/components/Home.vue
+++ templates/src/components/Home.vue
@@ -10,25 +10,6 @@ import Header from '../components/Header.vue'
 
 export default {
   name: 'Home',
-  data() {
-    return {
-      msg: '',
-    }
-  },
-  methods: {
-    getMessage() {
-      axios.get(this.$hostname)
-        .then((res) => {
-          this.msg = res.data
-        })
-        .catch((error) => {
-          console.error(error) // TODO: handle error correctly
-        })
-    },
-  },
-  created() {
-    this.getMessage()
-  },
   components: {
     Header
   }
