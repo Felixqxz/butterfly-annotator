77157b42c3090e5c541b8f5122a3c4811c238395
Xingzhi Qian <xq219@ic.ac.uk>
2021-10-18 16:19:07 +0100

    login backend works and start refactor

--- website/app.py
+++ website/app.py
@@ -1,15 +1,16 @@
-from flask import Flask
+from flask import Flask, request, flash, session
 from flask_cors import CORS
 from flask_login import LoginManager
-from flask_bcrypt import Bcrypt
 from .database.access import db
 from .config import config_by_name
 from .database.loaders import setup_user_loader
+from flask_login import login_user, current_user
+from .database.models import User
+from flask_bcrypt import Bcrypt
+from flask.json import jsonify
 
 # configuration
 DEBUG = True
-
-login_manager = LoginManager()
 
 def create_app(config_name=None):
     # create and configure the app
@@ -19,17 +20,18 @@ def create_app(config_name=None):
     else:
         app.config.from_object(config_by_name[config_name])
     db.init_app(app)
-    bcrypt = Bcrypt(app)
     # enable CORS
     CORS(app, resources={r'/*': {'origins': '*'}})
     # Add routes
     from .images.image_api import image_api
     app.register_blueprint(image_api)
     # Create & setup LoginManager
-    # login_manager = LoginManager()
+    login_manager = LoginManager()
     login_manager.init_app(app)
     login_manager.login_view = 'login'
     setup_user_loader(login_manager)
+
+    
     # Add CLI custom commands
     from .cli import create_all, drop_all
     app.cli.add_command(create_all)
@@ -38,6 +40,53 @@ def create_app(config_name=None):
     return app
 
 app = create_app()
+
+bcrypt = Bcrypt(app)
+app.secret_key = '766574a7486ff3209b5b2a347a854f168d0a5d2af588b681cf592fb7f61f99e2'
+
+@app.route('/register', methods=['POST'])
+def register():
+    user_info = request.get_json(force=True)
+    username = user_info.get("username")
+    email = user_info.get('email')
+    password = user_info.get('password') 
+
+    user = db.session.query(User).filter(User.username == username).first()
+    if user:
+        return jsonify({"status": 401,
+                        "reason": "Username already exists"})
+    else:
+        password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+        user = User(username=username, email=email, password_hash=password_hash)
+        db.session.add(user)
+        db.session.commit()
+        login_user(user)
+        flash('Logged in successfully.')
+        return jsonify({"status": 200})
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    if current_user.get_id() != None:
+        return jsonify({"status": 401,    
+                        "reason": "User already logged in"})
+
+    user_info = request.get_json(force=True)
+    username = user_info.get('username')
+    password = user_info.get('password') 
+    
+    user = db.session.query(User).filter(User.username == username).first()
+    if user:
+        if bcrypt.check_password_hash(user.password_hash, password):
+            login_user(user)
+            flash('Logged in successfully.')
+            return jsonify({"status": 200})
+        else:
+            return jsonify({"status": 401,    
+                        "reason": "Password incorrect"})
+    else:
+        return jsonify({"status": 401,    
+                        "reason": "Username or Password Error"})
 
 if __name__ == '__main__':
     app.run()
