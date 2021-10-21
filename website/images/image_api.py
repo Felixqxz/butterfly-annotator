9fa8fa1126684d6082b56d78c615945edf6485eb
olsdavis <olsdavis@gmail.com>
2021-10-21 15:08:52 +0100

    removing unused file

--- website/images/image_api.py
+++
@@ -1,9 +0,0 @@
-"""
-The part of the API that allows access to the images of a database.
-"""
-from flask.json import jsonify
-from flask import Blueprint
-
-image_api = Blueprint('image_api', __name__)
-
-
