dba0cff2bd8d9b1ae59a2849ebe8af6b025e63c2
yc1319 <yc1319@imperial.ac.uk>
2021-10-30 15:05:35 +0100

    alert auto dismissing

---
+++ templates/src/components/Alert.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <b-alert
+      :show="dismissCountDown"
+      dismissible
+      :variant=alertState
+      @dismissed="dismissCountDown=0"
+      @dismiss-count-down="countDownChanged"
+    >{{ message }}</b-alert>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Alert",
+  props: ['message', 'alertState'],
+  data() {
+    return {
+      dismissSecs: 3,
+      dismissCountDown: 0,
+    }
+  },
+  methods: {
+    countDownChanged(dismissCountDown) {
+        this.dismissCountDown = dismissCountDown
+      },
+      showAlert() {
+        this.dismissCountDown = this.dismissSecs
+      }
+  }
+}
+</script>
