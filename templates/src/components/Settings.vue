a3577a16edd4cd2f658a43dfe96042098100cb91
olsdavis <olsdavis@gmail.com>
2021-11-22 16:21:57 +0000

    disallow editing of usernames

--- templates/src/components/Settings.vue
+++ templates/src/components/Settings.vue
@@ -7,7 +7,13 @@
     </b-row>
     <b-row>
       <b-col md="8" xs="12">
-        
+        <b-form id="oscar">
+          <label for="username-input">Username</label>
+          <b-form-input type="text" id="username-input"
+                        disabled :value="currentUser.username" class="mb-2"/>
+          <label for="email-input">Email</label>
+          <b-form-input type="text" id="email-input" disabled :value="currentUser.email"/>
+        </b-form>
       </b-col>
       <b-col md="4" xs="12">
         <profile-picture :picture="$hostname + '/api/profile-picture/' + currentUser.username" :editable="true" />
@@ -37,3 +43,13 @@ export default {
   },
 }
 </script>
+
+<style>
+.form-control:disabled {
+  background-color: transparent;
+  cursor: text;
+  border-color: transparent;
+  box-shadow: none;
+  outline-color: transparent;
+}
+</style>
