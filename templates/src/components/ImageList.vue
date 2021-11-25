315269dcac000f521c8285a6b9eb9f7e25fc10d0
olsdavis <olsdavis@gmail.com>
2021-11-25 15:27:53 +0000

    implemented search bar

--- templates/src/components/ImageList.vue
+++ templates/src/components/ImageList.vue
@@ -13,13 +13,13 @@
         <form>
           <b-row>
             <b-col cols="10" class="mt-2">
-              <b-input-group>
+              <b-input-group @keydown.enter.prevent="">
                 <b-input-group-prepend>
                   <b-input-group-text>
                     <b-icon-search></b-icon-search>
                   </b-input-group-text>
                 </b-input-group-prepend>
-                <input type="text" class="form-control" placeholder="Search..."/>
+                <input type="text" class="form-control" placeholder="Search..." v-model="searchText"/>
               </b-input-group>
             </b-col>
             <b-col cols="2">
@@ -28,7 +28,7 @@
           </b-row>
         </form>
         <b-row>
-          <b-col md="4" sm="6" xs="12" v-for="(image, index) in images" v-bind:key="image.id">
+          <b-col md="4" sm="6" xs="12" v-for="(image, index) in imagesToShow" v-bind:key="image.id">
             <router-link :to="'/annotate/' + image.id">
               <!-- Do not use b-card: it creates automatically a b-card-body tag -->
               <div class="card card-hover no-drag image-to-annotate">
@@ -108,10 +108,19 @@ export default {
       selectedLevel: null,
       permissionOptions: [],
       targetUser: null,
+      searchText: '',
     }
   },
   computed: {
-    ...mapGetters({ userInfo: 'currentUser' })
+    ...mapGetters({ userInfo: 'currentUser' }),
+    imagesToShow() {
+      const search = this.searchText.trim()
+      if (search) {
+        return this.images.filter(image => image.fullDescription.toLowerCase().includes(search.toLowerCase()))
+      } else {
+        return this.images
+      }
+    },
   },
   methods: {
     ...mapActions({
@@ -155,14 +164,14 @@ export default {
     },
     fetchImageList() {
       this.listImages({bankId: this.$route.params.bankId})
-          .then(res => {
-            let data = res.data
-            this.images = data.images
-            this.bankName = data.bankName
-          })
-          .catch(err => {
-            handleError(this.$bvToast, 'Cannot load image list', `Cause ${err.response.data.message}`)
-          })
+        .then(res => {
+          let data = res.data
+          this.images = data.images
+          this.bankName = data.bankName
+        })
+        .catch(err => {
+          handleError(this.$bvToast, 'Cannot load image list', `Cause ${err.response.data.message}`)
+        })
     },
     relistAccesses() {
       this.listAccesses({bankId: this.$route.params.bankId}).then(res => {
