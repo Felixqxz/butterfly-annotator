3d6b99ae3c122a846204eaf169c7aebce8b3cc0e
Xingzhi Qian <xq219@ic.ac.uk>
2021-11-30 01:50:36 +0000

    reformat codes

--- website/textproc/proc.py
+++ website/textproc/proc.py
@@ -17,26 +17,29 @@ def load_word_list(p):
     return ls
 
 user_keywords_list = load_word_list(USER_KEYWORDS_SELECTION_PATH)
+user_keywords = []
+
+# add keywords if it is not in the bound of user_keywords_selection list
+def add_keywords(start_index, end_index, keywords):
+    not_in_bound = True
+    for keyword in user_keywords:
+        if start_index >= keyword['start'] and start_index <= keyword['end'] or \
+            end_index >= keyword['start'] and end_index <= keyword['end']:
+            not_in_bound = False
+            break
+    if not_in_bound:
+        keywords.append({'start': start_index, 'end': end_index})
 
 def get_keywords(adjectives, patterns, description):
     keywords = []
-    user_keywords = []
     word = None
     start_index = -1
     i = 0
     for user_keyword in user_keywords_list:
-        print(user_keyword)
         start_index = description.lower().find(user_keyword)
         if start_index != -1:
-            not_in_bound = True
             end_index = start_index + len(user_keyword)
-            for keyword in user_keywords:
-                if start_index >= keyword['start'] and start_index <= keyword['end'] or \
-                    end_index >= keyword['start'] and end_index <= keyword['end']:
-                    not_in_bound = False
-                    break
-            if not_in_bound:
-                user_keywords.append({'start': start_index, 'end': end_index})
+            add_keywords(start_index, end_index, user_keywords)
 
     start_index = -1
     while i < len(description):
@@ -48,33 +51,17 @@ def get_keywords(adjectives, patterns, description):
             if word in adjectives and start_index == -1:
                 start_index = i
             elif word in patterns and start_index != -1:
-                not_in_bound = True
                 end_index = i + len(word)
-                for keyword in user_keywords:
-                    if start_index >= keyword['start'] and start_index <= keyword['end'] or \
-                        end_index >= keyword['start'] and end_index <= keyword['end']:
-                        not_in_bound = False
-                        break
-                
-                if not_in_bound:
-                    keywords.append({'start': start_index, 'end': i + len(word)})
+                add_keywords(start_index, end_index, keywords)
                 start_index = -1
             i += len(word)
         elif (curr == ';' or curr == '.') and start_index != -1:
             # termination!
-            not_in_bound = True
             end_index = i
-            for keyword in user_keywords:
-                if start_index >= keyword['start'] and start_index <= keyword['end'] or \
-                    end_index >= keyword['start'] and end_index <= keyword['end']:
-                    not_in_bound = False
-                    break
-            if not_in_bound:
-                keywords.append({'start': start_index, 'end': i})
+            add_keywords(start_index, end_index, keywords)
             start_index = -1
             i += 1
         else:
             i += 1
-    print(user_keywords)
-    print(keywords)
-    return user_keywords + keywords
+    all_keywords = user_keywords + keywords
+    return all_keywords
